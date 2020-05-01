299e100cdac904fc1de5b8dfe5424e85ad9d44e1
Druhin Tarafder <druhinpro@gmail.com>
2020-05-01 17:34:11 +0530

    dark mode added

--- new CoronaInfo dashboard/corona-info/night.js
+++ new CoronaInfo dashboard/corona-info/night.js
@@ -1,4 +1,12 @@
 function myFunction() {
-    document.body.style.backgroundColor = "var(--bg-dark)";
+	//	if (document.getElementById("night").src == "assets/night-light.svg")
+	document.body.style.backgroundColor = "var(--bg-dark)";
 	
+	document.getElementById("top").style.backgroundColor = "var(--bg-dark)";
+	document.getElementById("card").style.backgroundColor = "var(--card-dark)";
+	
+	
+	document.getElementById("night").src = "assets/night-dark.svg";
+	document.getElementById("notification").src = "assets/notification-dark.svg";
+
 }
