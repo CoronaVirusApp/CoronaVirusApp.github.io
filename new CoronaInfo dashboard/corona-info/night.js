a7f1d399af1f5b5f25add26b0f07d801736be607
Druhin Tarafder <druhinpro@gmail.com>
2020-05-01 17:41:29 +0530

    local storage dark mode added

--- new CoronaInfo dashboard/corona-info/night.js
+++ new CoronaInfo dashboard/corona-info/night.js
@@ -1,12 +1,31 @@
 function myFunction() {
-	//	if (document.getElementById("night").src == "assets/night-light.svg")
-	document.body.style.backgroundColor = "var(--bg-dark)";
-	
-	document.getElementById("top").style.backgroundColor = "var(--bg-dark)";
-	document.getElementById("card").style.backgroundColor = "var(--card-dark)";
-	
-	
-	document.getElementById("night").src = "assets/night-dark.svg";
-	document.getElementById("notification").src = "assets/notification-dark.svg";
+
+	if (localStorage.getItem('Dark') == 1) {
+		
+		localStorage.setItem('Dark',0);
+
+		document.body.style.backgroundColor = "var(--bg-light)";
+
+		document.getElementById("top").style.backgroundColor = "var(--bg-light)";
+		document.getElementById("card").style.backgroundColor = "var(--card-light)";
+
+
+		document.getElementById("night").src = "assets/night-light.svg";
+		document.getElementById("notification").src = "assets/notification-light.svg";
+
+
+	} else {
+
+		localStorage.setItem('Dark',1);
+
+		document.body.style.backgroundColor = "var(--bg-dark)";
+
+		document.getElementById("top").style.backgroundColor = "var(--bg-dark)";
+		document.getElementById("card").style.backgroundColor = "var(--card-dark)";
+
+
+		document.getElementById("night").src = "assets/night-dark.svg";
+		document.getElementById("notification").src = "assets/notification-dark.svg";
+	}
 
 }
