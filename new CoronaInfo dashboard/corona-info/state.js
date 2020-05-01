148e5596e360fd1261140dec71e9437e14123c5a
Druhin Tarafder <druhinpro@gmail.com>
2020-05-01 20:08:51 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/state.js
+++ new CoronaInfo dashboard/corona-info/state.js
@@ -4,7 +4,7 @@
 //d1 = total deaths (in rank 1 state)
 
 
-const api_url = 'https://api.covid19india.org/data.json';
+var api_url2 = 'https://api.covid19india.org/data.json';
 
 async function getData() {
 	const response = await fetch(api_url);
@@ -15,18 +15,28 @@ async function getData() {
 
 	//rank1
 
-	document.getElementById("s1").innerHTML = "1.&nbsp;&nbsp;" + (data.statewise[1].state);
+	document.getElementById("s1-name").innerHTML = (data.statewise[1].state).toUpperCase();
 	//  console.log(data.statewise[1].state);
-	document.getElementById("t1").innerHTML = (data.statewise[1].confirmed);
+	document.getElementById("s1-total").innerHTML = (data.statewise[1].confirmed);
 	//  console.log(data.statewise[1].confirmed);
-	document.getElementById("r1").innerHTML = (data.statewise[1].recovered);
-	//	console.log(data.statewise[1].recovered);
-	document.getElementById("d1").innerHTML = (data.statewise[1].deaths);
+	document.getElementById("s1-deaths").innerHTML = (data.statewise[1].deaths);
 	//	console.log(data.statewise[1].deaths);
-
-
-
 	
+	
+	/******************/
+
+	//rank2
+
+	document.getElementById("s2-name").innerHTML = (data.statewise[2].state).toUpperCase();
+	//  console.log(data.statewise[2].state);
+	document.getElementById("s2-total").innerHTML = (data.statewise[2].confirmed);
+	//  console.log(data.statewise[2].confirmed);
+	document.getElementById("s2-deaths").innerHTML = (data.statewise[2].deaths);
+	//	console.log(data.statewise[2].deaths);
+
+
+
+
 }
 
 
