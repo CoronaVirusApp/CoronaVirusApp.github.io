18bfa2ee55d5d281c42403fe462ca3cc1ad47b05
Druhin Tarafder <druhinpro@gmail.com>
2020-05-01 20:16:25 +0530

    Add files via upload

--- new CoronaInfo dashboard/corona-info/state.js
+++ new CoronaInfo dashboard/corona-info/state.js
@@ -33,6 +33,102 @@ async function getData() {
 	//  console.log(data.statewise[2].confirmed);
 	document.getElementById("s2-deaths").innerHTML = (data.statewise[2].deaths);
 	//	console.log(data.statewise[2].deaths);
+	
+	
+	/******************/
+
+	//rank3
+
+	document.getElementById("s3-name").innerHTML = (data.statewise[3].state).toUpperCase();
+	//  console.log(data.statewise[3].state);
+	document.getElementById("s3-total").innerHTML = (data.statewise[3].confirmed);
+	//  console.log(data.statewise[3].confirmed);
+	document.getElementById("s3-deaths").innerHTML = (data.statewise[3].deaths);
+	//	console.log(data.statewise[3].deaths);
+	
+	
+	/******************/
+
+	//rank4
+
+	document.getElementById("s4-name").innerHTML = (data.statewise[4].state).toUpperCase();
+	//  console.log(data.statewise[4].state);
+	document.getElementById("s4-total").innerHTML = (data.statewise[4].confirmed);
+	//  console.log(data.statewise[4].confirmed);
+	document.getElementById("s4-deaths").innerHTML = (data.statewise[4].deaths);
+	//	console.log(data.statewise[4].deaths);
+	
+	
+	/******************/
+
+	//rank5
+
+	document.getElementById("s5-name").innerHTML = (data.statewise[5].state).toUpperCase();
+	//  console.log(data.statewise[1].state);
+	document.getElementById("s5-total").innerHTML = (data.statewise[5].confirmed);
+	//  console.log(data.statewise[1].confirmed);
+	document.getElementById("s5-deaths").innerHTML = (data.statewise[5].deaths);
+	//	console.log(data.statewise[1].deaths);
+	
+	
+	/******************/
+
+	//rank6
+
+	document.getElementById("s6-name").innerHTML = (data.statewise[6].state).toUpperCase();
+	//  console.log(data.statewise[2].state);
+	document.getElementById("s6-total").innerHTML = (data.statewise[6].confirmed);
+	//  console.log(data.statewise[2].confirmed);
+	document.getElementById("s6-deaths").innerHTML = (data.statewise[6].deaths);
+	//	console.log(data.statewise[2].deaths);
+	
+	
+	/******************/
+
+	//rank7
+
+	document.getElementById("s7-name").innerHTML = (data.statewise[7].state).toUpperCase();
+	//  console.log(data.statewise[3].state);
+	document.getElementById("s7-total").innerHTML = (data.statewise[7].confirmed);
+	//  console.log(data.statewise[3].confirmed);
+	document.getElementById("s7-deaths").innerHTML = (data.statewise[7].deaths);
+	//	console.log(data.statewise[3].deaths);
+	
+	
+	/******************/
+
+	//rank8
+
+	document.getElementById("s8-name").innerHTML = (data.statewise[8].state).toUpperCase();
+	//  console.log(data.statewise[4].state);
+	document.getElementById("s8-total").innerHTML = (data.statewise[8].confirmed);
+	//  console.log(data.statewise[4].confirmed);
+	document.getElementById("s8-deaths").innerHTML = (data.statewise[8].deaths);
+	//	console.log(data.statewise[4].deaths);
+	
+	
+	/******************/
+
+	//rank9
+
+	document.getElementById("s9-name").innerHTML = (data.statewise[9].state).toUpperCase();
+	//  console.log(data.statewise[3].state);
+	document.getElementById("s9-total").innerHTML = (data.statewise[9].confirmed);
+	//  console.log(data.statewise[3].confirmed);
+	document.getElementById("s9-deaths").innerHTML = (data.statewise[9].deaths);
+	//	console.log(data.statewise[3].deaths);
+	
+	
+	/******************/
+
+	//rank10
+
+	document.getElementById("s10-name").innerHTML = (data.statewise[10].state).toUpperCase();
+	//  console.log(data.statewise[4].state);
+	document.getElementById("s10-total").innerHTML = (data.statewise[10].confirmed);
+	//  console.log(data.statewise[4].confirmed);
+	document.getElementById("s10-deaths").innerHTML = (data.statewise[10].deaths);
+	//	console.log(data.statewise[4].deaths);
 
 
 
